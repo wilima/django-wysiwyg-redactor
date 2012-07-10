06f91ea40084b69eb60ea5a74a5f5dd8cf73964f
Douglas Miranda <douglasmirandasilva@gmail.com>
2012-07-10 17:51:29 -0400

    back to the original code widgets.py and django_admin.css

--- redactor/widgets.py
+++ redactor/widgets.py
@@ -9,9 +9,6 @@ GLOBAL_OPTIONS = getattr(settings, 'REDACTOR_OPTIONS', {})
 
 INIT_JS = """<script type="text/javascript">
   jQuery(document).ready(function(){
-    $(".redactor_content").each(function (i) {
-        $(this).parent("div").parent("div").find("label").addClass("redactor_label");
-    });
     $("#%s").redactor(%s);
   });
 </script>
@@ -47,14 +44,9 @@ class RedactorEditor(widgets.Textarea):
         return json.dumps(options)
 
     def render(self, name, value, attrs=None):
-        html_class_name = attrs.get('class', '')
-        redactor_class = html_class_name and " redactor_content" or "redactor_content"
-        html_class_name += redactor_class
-        attrs['class'] = html_class_name
+        html = super(RedactorEditor, self).render(name, value, attrs)
         final_attrs = self.build_attrs(attrs)
         id_ = final_attrs.get('id')
-
-        html = super(RedactorEditor, self).render(name, value, attrs)
         html += INIT_JS % (id_, self.get_options())
         return mark_safe(html)
 
