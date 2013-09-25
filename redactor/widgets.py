921af4a6c4114ec50afa166b3fc38b28ee9cfa17
Douglas Miranda <douglasmirandasilva@gmail.com>
2013-09-25 03:07:53 -0400

    cleaning: trailing spaces and code format

--- redactor/widgets.py
+++ redactor/widgets.py
@@ -41,11 +41,15 @@ class RedactorEditor(widgets.Textarea):
         options = GLOBAL_OPTIONS.copy()
         options.update(self.custom_options)
         if self.allow_file_upload:
-            options['fileUpload'] = reverse('redactor_upload_file', 
-                kwargs={'upload_to': self.upload_to})
+            options['fileUpload'] = reverse(
+                'redactor_upload_file',
+                kwargs={'upload_to': self.upload_to}
+            )
         if self.allow_image_upload:
-            options['imageUpload'] = reverse('redactor_upload_image', 
-                kwargs={'upload_to': self.upload_to})
+            options['imageUpload'] = reverse(
+                'redactor_upload_image',
+                kwargs={'upload_to': self.upload_to}
+            )
         return json.dumps(options)
 
     def render(self, name, value, attrs=None):
