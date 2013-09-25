b4786d61979f726ad9ddbc2a8751279edbb7da7d
Douglas Miranda <douglasmirandasilva@gmail.com>
2013-09-25 03:05:24 -0400

    better code format

--- redactor/urls.py
+++ redactor/urls.py
@@ -10,13 +10,21 @@ from redactor.forms import FileForm, ImageForm
 
 urlpatterns = patterns(
     '',
-    url('^upload/image/(?P<upload_to>.*)', redactor_upload, {
-        'form_class': ImageForm,
-        'response': lambda name, url: '<img src="%s" alt="%s" />' % (url, name),
-        }, name='redactor_upload_image'),
+    url('^upload/image/(?P<upload_to>.*)', redactor_upload,
+        {
+            'form_class': ImageForm,
+            'response': lambda name, url: '<img src="{0}" alt="{1}" />'.format(
+                url,
+                name
+            ),
+        },
+        name='redactor_upload_image'),
 
-    url('^upload/file/(?P<upload_to>.*)', redactor_upload, {
-        'form_class': FileForm,
-        'response': lambda name, url: '<a href="%s">%s</a>' % (url, name),
-        }, name='redactor_upload_file'),
+    url('^upload/file/(?P<upload_to>.*)', redactor_upload,
+        {
+            'form_class': FileForm,
+            'response': lambda name, url: '<a href="{0}">{1}</a>'.format(url,
+                                                                         name),
+        },
+        name='redactor_upload_file'),
 )
