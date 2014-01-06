48ee5eccbe2beaca0d1d24dfd165a1f00964a3c7
Douglas Miranda <douglasmirandasilva@gmail.com>
2014-01-06 02:44:47 -0300

    Change README file name on setup.py and MANIFEST.in.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
 readme = f.read()
 f.close()
 
