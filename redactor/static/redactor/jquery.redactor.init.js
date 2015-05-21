cb2597dd8aca331cf7f5368f9fdb71db683bbea1
Nikhil Benesch <nikhil.benesch@gmail.com>
2015-05-21 00:06:02 -0400

    Support additional jQuery instances loaded after widget
    When another jQuery instance is loaded after the Redactor widget,
    `window.jQuery` is clobbered with a jQuery *without* the Redactor
    plugin. This happens commonly in complex admin forms, where another
    widget includes its own version of jQuery. Since our event handlers
    refer to `window.jQuery` at the time the event handler is run, and not
    the time the event handler was bound, our event handlers use a jQuery
    without the Redactor plugin.
    
    Wrap the event handler initialization in a closure that retains the
    value of `window.jQuery` at the time the Redactor widget is loaded.

--- redactor/static/redactor/jquery.redactor.init.js
+++ redactor/static/redactor/jquery.redactor.init.js
@@ -9,27 +9,28 @@ so it can be used as part of an inline formset.
 Credit to the approach taken in:
 https://github.com/yourlabs/django-autocomplete-light
 **/
-jQuery(document).ready(function() {
-    jQuery('textarea.redactor-box').on('initialize', function() {
-        redactor_options = jQuery(this).data('redactor-options');
-        redactor_options.imageUploadErrorCallback = function (json) {
-            // TODO: Needs better error messages
-            alert(json.error);
-        }
-        jQuery(this).redactor(redactor_options);
+(function($) {
+    $(document).ready(function() {
+        $('textarea.redactor-box').on('initialize', function() {
+            redactor_options = $(this).data('redactor-options');
+            redactor_options.imageUploadErrorCallback = function (json) {
+                // TODO: Needs better error messages
+                alert(json.error);
+            }
+            $(this).redactor(redactor_options);
+        });
+        $(document).trigger('redactorWidgetReady');
+
+        $('textarea.redactor-box:not([id*="__prefix__"])').each(function() {
+            $(this).trigger('initialize');
+        });
+
+        $(document).bind('DOMNodeInserted', function(e) {
+            var widget = $(e.target).find('.redactor-box');
+
+            if (!widget.length) return;
+
+            widget.trigger('initialize');
+        });
     });
-    jQuery(document).trigger('redactorWidgetReady');
-
-    jQuery('textarea.redactor-box:not([id*="__prefix__"])').each(function() {
-        jQuery(this).trigger('initialize');
-    });
-
-     jQuery(document).bind('DOMNodeInserted', function(e) {
-        var widget = jQuery(e.target).find('.redactor-box');
-
-        if (!widget.length) return;
-
-        widget.trigger('initialize');
-    });
-
-});
+})(jQuery);
