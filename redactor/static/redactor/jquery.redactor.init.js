335747ef32ea55dd70d6f05d4268c749511ce5e4
Nikhil Benesch <nikhil.benesch@gmail.com>
2015-05-21 14:10:02 -0400

    Fix admin inline dynamic binding
    Previously, the `initialize` event was only defined on pre-existing
    textareas; triggering `initialize` on dynamically-added textareas had no
    effect. Define it instead for all `textarea.redactor-box`es, present and
    future.
    
    Also use `.add-row`s on-click handler to detect new inlines rather than
    the DOMNodeInserted event. DOM mutations events are deprecated, not
    supported in old versions of IE, and cause substantial overhead. This
    also prevents double-initializing textareas that are not part of the
    inline system.

--- redactor/static/redactor/jquery.redactor.init.js
+++ redactor/static/redactor/jquery.redactor.init.js
@@ -2,16 +2,9 @@ if (typeof jQuery === 'undefined' && django && django.jQuery) {
     jQuery = django.jQuery;
 }
 
-/**
-This makes sure that we initialise the redactor on the text area once its displayed
-so it can be used as part of an inline formset.
-
-Credit to the approach taken in:
-https://github.com/yourlabs/django-autocomplete-light
-**/
 (function($) {
     $(document).ready(function() {
-        $('textarea.redactor-box').on('initialize', function() {
+        $(document).on('redactor:init', 'textarea.redactor-box', function() {
             redactor_options = $(this).data('redactor-options');
             redactor_options.imageUploadErrorCallback = function (json) {
                 // TODO: Needs better error messages
@@ -22,15 +15,20 @@ https://github.com/yourlabs/django-autocomplete-light
         $(document).trigger('redactorWidgetReady');
 
         $('textarea.redactor-box:not([id*="__prefix__"])').each(function() {
-            $(this).trigger('initialize');
+            $(this).trigger('redactor:init');
         });
 
-        $(document).bind('DOMNodeInserted', function(e) {
-            var widget = $(e.target).find('.redactor-box');
-
-            if (!widget.length) return;
-
-            widget.trigger('initialize');
+        // Initialize Redactor on admin's dynamically-added inline
+        // formsets.
+        //
+        // Credit to the approach taken in django-selectable:
+        // https://github.com/mlavin/django-selectable
+        $(document).on('click', '.add-row', function () {
+            $(this)
+                .parents('.inline-related')
+                .find('.form-row:not(.empty-form)').last()
+                .find('textarea.redactor-box')
+                .trigger('redactor:init');
         });
     });
 })(jQuery);
