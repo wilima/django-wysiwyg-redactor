9263821eace164b201b64899fd675fd200f88069
Tomáš Mikula <mail@tomasmikula.cz>
2016-03-01 15:59:52 +0100

    url fix

--- redactor/urls.py
+++ redactor/urls.py
@@ -9,7 +9,6 @@ from redactor.forms import FileForm, ImageForm
 
 
 urlpatterns = [
-    '',
     url(r'^upload/image/(?P<upload_to>.*)',
         RedactorUploadView.as_view(form_class=ImageForm),
         name='redactor_upload_image'),
